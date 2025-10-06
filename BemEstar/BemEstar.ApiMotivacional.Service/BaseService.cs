a94c1a43ad6c0407cd1096431d49cdff7ecfad68
Daniela2319 <frankvt04@gmail.com>
2025-10-06 13:40:50 -0300

    feat(db): CRUD com postgres

--- BemEstar/BemEstar.ApiMotivacional.Service/BaseService.cs
+++ BemEstar/BemEstar.ApiMotivacional.Service/BaseService.cs
@@ -7,29 +7,29 @@ namespace BemEstar.ApiMotivacional.Service
     public class BaseService<T> : IService<T> where T : BaseModel
     {
         public static List<T> list  = new List<T>();
-        public void Create(T model)
+        public virtual void Create(T model)
         {
             list.Add(model);
         }
 
-        public void Delete(int id)
+        public virtual void Delete(int id)
         {
             T item = this.ReadById(id);
             list.Remove(item);
         }
 
-        public List<T> Read()
+        public virtual List<T> Read()
         {
             return list;
         }
 
-        public T ReadById(int id)
+        public virtual T ReadById(int id)
         {
             T item = list.FirstOrDefault(i => i.Id == id);
             return item;
         }
 
-        public void Update(T model)
+        public virtual void Update(T model)
         {
             T olditem = this.ReadById(model.Id);
             this.Delete(olditem.Id);
