14757151b578add5bcfcbdc8858648511eb2d8da
Daniela2319 <frankvt04@gmail.com>
2025-10-20 20:08:03 -0300

    feat: utilizando appsetting.json e user_secrets

--- BemEstar/BemEstar.ApiMotivacional.Service/DataBase.cs
+++ BemEstar/BemEstar.ApiMotivacional.Service/DataBase.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Npgsql;
 
 namespace BemEstar.ApiMotivacional.Service
 {
     internal class DataBase
     {
-        private readonly string _connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING_POSTGRES");
-            
+        private readonly string _connectionString;
 
+        public DataBase()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets<DataBase>()
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+            this._connectionString = configuration.GetConnectionString("Postgres");
+        }
         public NpgsqlConnection GetConnection()
         {
             NpgsqlConnection connection = new NpgsqlConnection(_connectionString);
